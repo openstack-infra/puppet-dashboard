8f77a5fd4e0722c67570318b47851ce70ddc6063
Dan Bode <dan@puppetlabs.com>
2011-11-17 22:44:13 -0800

    (#10947) fix update tests so that they pass

--- spec/unit/face/dashboard_spec.rb
+++ spec/unit/face/dashboard_spec.rb
@@ -3,7 +3,7 @@ require 'puppet'
 require 'puppet/face'
 describe Puppet::Face[:dashboard, :current] do
   let :dashboard_options do
-    {:enc_server => 'enc_server', :enc_port => '3001'}
+    {:enc_server => 'enc_server', :enc_port => '3001', :enc_auth_user => 'user', :enc_auth_passwd => 'pw'}
   end
   let :connection do
     mock('Puppet::Dashboard::Classifier')
@@ -11,7 +11,7 @@ describe Puppet::Face[:dashboard, :current] do
 
   describe 'action list' do
     it 'should default enc_server to Puppet[:server] and port to 3000' do
-      defaults = {:enc_server =>'master', :enc_port => 3000}
+      defaults = {:enc_server =>'master', :enc_port => 3000, :enc_auth_user => nil, :enc_auth_passwd => nil}
       Puppet.expects(:[]).with(:server).returns('master')
       Puppet::Dashboard::Classifier.expects(:connection).with(defaults).returns connection
       connection.expects(:list).with('node_classes', 'Listing classes')
@@ -28,19 +28,19 @@ describe Puppet::Face[:dashboard, :current] do
       expect { subject.list('foo', {} ) }.should raise_error(Puppet::Error, /Invalid type specified/)
     end
   end
-  describe 'actions create_class and create_node' do
-    {'node' => 'node', 'class' => 'node_class'}.each do |type,dash_type|
-      it "should require the name option for #{type}" do
-        expect { subject.send("create_#{type}", {}) }.should raise_error(ArgumentError)
-      end
-      it "should accept name option for #{type}" do
-        munged_options = dashboard_options.merge(:name => 'dan')
-        Puppet::Dashboard::Classifier.expects(:connection).with(munged_options).returns connection
-        connection.expects(:create).with(dash_type, "Creating #{type} dan", { dash_type => { 'name' => 'dan' } })
-        subject.send("create_#{type}", munged_options)
-      end
-    end
+  #describe 'actions create_class and create_node' do
+  #  {'node' => 'node', 'class' => 'node_class'}.each do |type,dash_type|
+  it "should require the name option for class" do
+    expect { subject.send("create_class", {}) }.should raise_error(ArgumentError)
   end
+  it "should accept name option for class" do
+    munged_options = dashboard_options.merge(:name => 'dan')
+    Puppet::Dashboard::Classifier.expects(:connection).with(munged_options).returns connection
+    connection.expects(:create_classes).with(['dan'])
+    subject.send("create_class", munged_options)
+  end
+  #  end
+  #end
   describe 'action register_module' do
 
   end
